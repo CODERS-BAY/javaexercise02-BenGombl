2e3e9ad966f393a7118b2ba4fba73c7c75f015c2
Ben Gombl <gomboc95@gmx.at>
2020-08-05 12:41:58 +0200

    fsf

---
+++ src/JavaExercise02.java
@@ -0,0 +1,48 @@
+public class JavaExercise02 {
+    public static void main(String[] args) {
+        boolean a, b, c;
+        a = true;
+        b = true;
+        c = true;
+
+
+        System.out.println("   A   -   B   -   C   >  statement");
+        System.out.println("---------------------------------");
+
+        do {
+            a = !a;
+            do {
+                b = !b;
+                do {
+                    c = !c;
+
+                    if (a) {
+                        System.out.print(" ");
+                    }
+
+                    System.out.print("(" + a + ") (");
+
+                    if (b) {
+                        System.out.print(" ");
+                    }
+
+                    System.out.print(b + ") (");
+
+                    if (c) {
+                        System.out.print(" ");
+                    }
+                    System.out.print(c + ") > (");
+                    //hier den Ausdruck zum überprüfen ändern!!!!!------------------------------------------------------
+                    System.out.println((!a||b) + ")");
+
+                    System.out.println("---------------------------------");
+                }
+                while (c == false);
+            }
+            while (b == false);
+        }
+        while (a == false);
+
+    }
+
+}
